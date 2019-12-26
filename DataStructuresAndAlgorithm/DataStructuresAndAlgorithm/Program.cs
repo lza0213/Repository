d376ee7a91a9c81cce236ba00b754368ef73f1ab
lza0213 <1827916843@qq.com>
2019-12-26 23:31:35 +0800

    添加解决方案：查找关键字“人工智能”

--- DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/Program.cs
@@ -10,7 +10,8 @@ namespace DataStructuresAndAlgorithm
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
